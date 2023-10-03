d4aabb2a30de5607867410940c87d3dea9adc962
LeoDeSilva <leo.desilvafamily@gmail.com>
2023-10-03 21:52:39 +0100

    compile single dimension infix operations, create unit tests for compiler

--- src/compiler/compiler.rs
+++ src/compiler/compiler.rs
@@ -17,6 +17,7 @@ impl Compiler {
         for (value, label) in &self.constants {
             out = out + &format!("{label} dat {value}\n");
         }
+        println!("{}", out);
         out
     }
 
@@ -26,11 +27,19 @@ impl Compiler {
             Node::DECLARATION(identifier, expression) => { self.compile_declaration(*identifier, *expression) }
             Node::INFIX(lhs, op, rhs) => { self.compile_infix(*lhs, op, *rhs) }
 
-            Node::NUMBER(value) => { self.compile_number(value) },
+            Node::NUMBER(value) => { "lda ".to_owned() + &self.compile_number_literal(value) },
+            Node::IDENTIFIER(identifier) => { "lda ".to_owned() + &self.compile_identifier_literal(identifier) },
 
             _ => { panic!("Unexpected node found in compile_node(), got: {:?}", node)}
         }
-    }
+   }
+
+   fn compile_atom(&mut self, atom: Node) -> String {
+        match atom {
+            Node::NUMBER(value) => { self.compile_number_literal(value) },
+            _ => { panic!("Unexpected node found in compile_node(), got: {:?}", atom)}
+        }
+   }
 
     fn compile_block(&mut self, statements: Vec<Node>) -> String {
         let mut out: String = String::new();
@@ -44,20 +53,63 @@ impl Compiler {
     fn compile_declaration(&mut self, identifier_node: Node, expression_node: Node) -> String {
         let identifier = if let Node::IDENTIFIER(identifier) = &identifier_node { identifier } else { panic!("Parser error, expected identifier type: IDENTIFIER, got: {:?}", identifier_node) };
         let expression = self.compile_node(expression_node);
-        format!("{identifier} dat 0\n{expression} \nsta {identifier}\n")
+        format!("{identifier} dat 0\n{expression}\nsta {identifier}\n")
 
     }
 
-    fn compile_infix(&mut self, lhs: Node, op: Token, rhs: Node) -> String {
-        String::new()
+    fn compile_infix(&mut self, lhs_node: Node, op_tok: Token, rhs_node: Node) -> String {
+        let lhs = self.compile_node(lhs_node);
+        let rhs = self.compile_atom(rhs_node);
+        format!("{lhs}\n{:?} {rhs}", op_tok)
     }
 
-    fn compile_number(&mut self, value: i32) -> String {
+    fn compile_number_literal(&mut self, value: i32) -> String {
         if self.constants.contains_key(&value) {
-            "lda ".to_owned() + &self.constants.get(&value).unwrap().clone()
+            self.constants.get(&value).unwrap().clone()
         } else {
             self.constants.insert(value, "_".to_owned() + &value.to_string());
-            "lda ".to_owned() + &self.constants.get(&value).unwrap().clone()
+            self.constants.get(&value).unwrap().clone()
         }
+    }
+
+    fn compile_identifier_literal(&mut self, identifier: String) -> String {
+        //TODO CHECK identifier exists and is initialised
+        identifier
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compile_declaration() {
+        let mut c = Compiler::new();
+        assert_eq!(c.compile(Node::BLOCK(Box::new(vec![
+            Node::DECLARATION(
+                Box::new(Node::IDENTIFIER(String::from("x"))), 
+                Box::new(Node::NUMBER(1)),
+            )]
+        ))), 
+
+        String::from("x dat 0\nlda _1\nsta x\n_1 dat 1\n")
+        );
+    }
+
+    #[test]
+    fn test_compile_infix() {
+        let mut c = Compiler::new();
+        let out: String = c.compile(Node::BLOCK(Box::new(vec![
+            Node::DECLARATION(
+                Box::new(Node::IDENTIFIER(String::from("x"))), 
+                Box::new(Node::INFIX(
+                    Box::new(Node::NUMBER(1)), 
+                    Token::ADD, 
+                    Box::new(Node::NUMBER(2)))),
+            )]
+        )));
+
+        assert_eq!(out[0..28], 
+        String::from("x dat 0\nlda _1\nADD _2\nsta x\n"));
     }
 }
